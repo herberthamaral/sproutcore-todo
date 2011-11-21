d7fd93900c8460765087187aa8b37fbe077bd54d
Herberth Amaral <herberthamaral@gmail.com>
2011-11-21 10:41:39 -0200

    adding LoginSample

---
+++ apps/login_sample/controllers/login_controller.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   LoginSample.loginController
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals LoginSample */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+LoginSample.loginController = SC.ObjectController.create(
+/** @scope LoginSample.loginController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/login_sample/tests/controllers/login_test.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   LoginSample.loginController Unit Test
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals LoginSample module test ok equals same stop start */
+
+module("LoginSample.loginController");
+
+// TODO: Replace with real unit test for LoginSample.loginController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
